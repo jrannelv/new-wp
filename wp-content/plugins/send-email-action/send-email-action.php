d82e24e3ebbbef8d020300f52cfccf41f2ebfcea
John Rannel Valdez <john.valdez@getpantheon.com>
2022-07-08 05:55:50 +0000

    send-email-action

---
+++ wp-content/plugins/send-email-action/send-email-action.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Send Email Action
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     send-email-action
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Send_Email_Action
+ */
+
+// Your code starts here.
